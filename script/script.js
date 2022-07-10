2f624a1f856f4efa14528b4e32e405f26cd2e0e3
Chonkust <jurgis1@icloud.com>
2022-07-10 15:14:24 +0100

    JS update

--- script/script.js
+++ script/script.js
@@ -1,5 +1,26 @@
 // Executing in 'strict' mode
-"use strict";
+// "use strict";
 document.addEventListener("deviceready", onDeviceReady, false);
 
 // Function list
+
+// Drop-down menu item
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
