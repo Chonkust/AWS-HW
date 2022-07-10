92e89a882023f46e068b603c89312d8e1d7b9188
Chonkust <jurgis1@icloud.com>
2022-07-10 15:06:01 +0100

    Updated header

--- script/script.js
+++ script/script.js
@@ -1,23 +1,5 @@
 // Executing in 'strict' mode
 "use strict";
-
-
 document.addEventListener("deviceready", onDeviceReady, false);
 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// Function list
